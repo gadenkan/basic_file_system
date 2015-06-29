625426ce3e578c9d9dee82ebc2c97c62918e58ad
punitmg <punitmg@gmail.com>
2015-06-29 11:54:25 -0700

    adding custom Error message

--- basic_file_system/src/com/interview/filesystem/FileSystem.java
+++ basic_file_system/src/com/interview/filesystem/FileSystem.java
@@ -21,46 +21,35 @@ public class FileSystem {
 			String line = reader.readLine();
 			while(line != null){
 				
+				System.out.println(line);
 				if(line.indexOf("create ") == 0){
 					boolean result = createFile(line.substring(7));
-					if(result){
-						System.out.println(line);
-					}else{
+					if(!result){
 						System.out.println("Error creating file. Command "+line);
 					}
 				}else if(line.indexOf("mkdir ") == 0){
 					boolean result = createDirectory(line.substring(6));
-					if(result){
-						System.out.println(line);
-					}else{
+					if(!result){
 						System.out.println("Error creating directory. Command "+line);
 					}
 				}else if(line.indexOf("append ") == 0){
 					boolean result = appendToFile(line.substring(7));
-					if(result){
-						System.out.println(line);
-					}else{
+					if(!result){
 						System.out.println("Error appending string to file. Command "+line);
 					}
 				}else if(line.indexOf("cp ") == 0){
 					boolean result = copyFile(line.substring(3));
-					if(result){
-						System.out.println(line);
-					}else{
+					if(!result){
 						System.out.println("Error copying file. Command "+line);
 					}
 				}else if(line.indexOf("rm ") == 0){
 					boolean result = removeFile(line.substring(3));
-					if(result){
-						System.out.println(line);
-					}else{
+					if(!result){
 						System.out.println("Error removing file. Command "+line);
 					}
 				}else if(line.indexOf("cat ") == 0){
 					boolean result = concatenate(line.substring(4));
-					if(result){
-						System.out.println(line);
-					}else{
+					if(!result){
 						System.out.println("Error reading file. Command "+line);
 					}
 				}
@@ -68,6 +57,7 @@ public class FileSystem {
 				line = reader.readLine();
 			}
 			
+			System.out.println("\n");
 			displayDirectoryContents();
 		}finally {
 			reader.close();
@@ -100,9 +90,7 @@ public class FileSystem {
 
 	private static boolean concatenate(String command) {
 
-		System.out.println(command);
 		String[] strArr = command.split("/");
-		System.out.println(strArr.length);
 			
 		// find directory
 		// when the correct directory is reached
@@ -115,16 +103,14 @@ public class FileSystem {
 			System.out.println(f.getContents());
 			return true;
 		}catch(Exception e){
-			e.printStackTrace();
+			System.out.println(e.getMessage());
 			return false;
 		}
 	}
 
 	private static boolean removeFile(String command) {
 
-		System.out.println(command);
 		String[] strArr = command.split("/");
-		System.out.println(strArr.length);
 			
 		// find directory
 		// when the correct directory is reached
@@ -134,20 +120,18 @@ public class FileSystem {
 			Directory d = findDirectory(strArr);
 			
 			d.removeFile(strArr[strArr.length-1]);
-			System.out.println("Removed file "+strArr[strArr.length-1]+" to directory "+d.getName());
+			//System.out.println("Removed file "+strArr[strArr.length-1]+" to directory "+d.getName());
 			return true;
 		}catch(Exception e){
-			e.printStackTrace();
+			System.out.println("Error "+e.getMessage());
 			return false;
 		}
 	}
 
 	private static boolean copyFile(String command) {
 
-		System.out.println(command);
 		
 		String[] strArr0 = command.split(" ");
-		System.out.println("strArr0 1: "+strArr0[1]);
 		
 		String[] strArr1 = strArr0[0].split("/");
 		String[] strArr2 = strArr0[1].split("/");
@@ -166,22 +150,19 @@ public class FileSystem {
 			File f2 = new File(strArr2[strArr2.length-1], f);
 			d2.addFile(f2);
 			f2.setDirectory(d2);
-			System.out.println("Copied of file "+f.getName()+ " to "+f2.getName());
+			//System.out.println("Copied of file "+f.getName()+ " to "+f2.getName());
 			return true;
 		}catch(Exception e){
-			e.printStackTrace();
+			System.out.println(e.getMessage());
 			return false;
 		}
 	}
 
 	private static boolean appendToFile(String command) {
 
-		System.out.println(command);
 		
 		String[] strArr0 = command.split(" ");
-		System.out.println("strArr0 1: "+strArr0[1]);
 		String[] strArr1 = strArr0[1].split("/");
-		System.out.println(strArr0.length);
 			
 		// find directory
 		// when the correct directory is reached
@@ -193,18 +174,16 @@ public class FileSystem {
 			File f = d.getFile(strArr1[strArr1.length-1]);
 			
 			f.appendContent(strArr0[0]);
-			System.out.println("Contents of file "+f.getName()+ " : "+f.getContents());
+			//System.out.println("Contents of file "+f.getName()+ " : "+f.getContents());
 			return true;
 		}catch(Exception e){
-			e.printStackTrace();
+			System.out.println(e.getMessage());
 			return false;
 		}
 	}
 
 	public static boolean createFile(String command){
-		System.out.println(command);
 		String[] strArr = command.split("/");
-		System.out.println(strArr.length);
 			
 		// find directory
 		// when the correct directory is reached
@@ -215,10 +194,10 @@ public class FileSystem {
 			
 			File f = new File(strArr[strArr.length-1]);
 			d.addFile(f);
-			System.out.println("Added file "+f.getName()+" to directory "+d.getName());
+			//System.out.println("Added file "+f.getName()+" to directory "+d.getName());
 			return true;
 		}catch(Exception e){
-			e.printStackTrace();
+			System.out.println(e.getMessage());
 			return false;
 		}
 	}
@@ -230,7 +209,7 @@ public class FileSystem {
 			try{
 				currDir = currDir.getChildDirectory(strArr[i]);
 			}catch(Exception e){
-				e.printStackTrace();
+				System.out.println(e.getMessage());
 				throw e;
 			}
 		}
@@ -238,9 +217,7 @@ public class FileSystem {
 	}
 
 	public static boolean createDirectory(String command){
-		System.out.println(command);
 		String[] strArr = command.split("/");
-		System.out.println(strArr.length);
 			
 		// find directory
 		// when the correct directory is reached
@@ -251,10 +228,10 @@ public class FileSystem {
 			
 			Directory newDir = new Directory(strArr[strArr.length-1], d);
 			d.addChildDirectory(newDir);
-			System.out.println("Added new directory "+newDir.getName()+" to directory "+d.getName());
+			//System.out.println("Added new directory "+newDir.getName()+" to directory "+d.getName());
 			return true;
 		}catch(Exception e){
-			e.printStackTrace();
+			System.out.println(e.getMessage());
 			return false;
 		}
 	}
